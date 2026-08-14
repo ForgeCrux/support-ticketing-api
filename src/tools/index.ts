31b9d248f040ab4543f79dcee51fd6496c56e5ef
Himanshu-KRE <himanshu.nag@probestack.io>
2026-08-14 15:42:18 +0530

    Push from ForgeSphere MCP wizard

---
+++ src/tools/index.ts
@@ -0,0 +1,9 @@
+// Re-export all tool handlers.
+export { searchticketsHandler } from "./searchtickets.js";
+export { createticketHandler } from "./createticket.js";
+export { getticketbyidHandler } from "./getticketbyid.js";
+export { updateticketHandler } from "./updateticket.js";
+export { closeticketHandler } from "./closeticket.js";
+export { addticketreplyHandler } from "./addticketreply.js";
+export { escalateticketHandler } from "./escalateticket.js";
+export { getcustomerprofileHandler } from "./getcustomerprofile.js";
